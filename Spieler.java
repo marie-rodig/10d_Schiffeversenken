0319fefad54d9cb1edad22d9fc595d80ba5fb67f
Sydney10d <sydney.frentzel04@gmail.com>
2021-06-16 08:37:52 +0200

    Gebiet programmiert

--- Spieler.java
+++ Spieler.java
@@ -3,35 +3,35 @@
 public class Spieler
 {
     abstract class Spieler implements Mode {
-	protected Spielfeld sp;
-	private int hoehe,breite;
+    protected Spielfeld sp;
+    private int hoehe,breite;
 
-	public Spieler() {
-	}
-	
-	public Spieler(Spieler sp1) {
-		this(sp1.getSpielfeld());
-	}
-	
-	public int Spielfeld sp (int y) {
-		if(sp != null) {
-			this.sp = sp;
-			hoehe = sp.getHoehe();
-			breite = sp.getBreite();
-		}
-	}
-	
-	abstract public void init(int mode);
+    public Spieler() {
+    }
+    
+    public Spieler(Spieler sp1) {
+        this(sp1.getSpielfeld());
+    }
+    
+    public int Spielfeld sp (int y) {
+        if(sp != null) {
+            this.sp = sp;
+            hoehe = sp.getHoehe();
+            breite = sp.getBreite();
+        }
+    }
+    
+    abstract public void init(int mode);
 
-	public Spielfeld getSpielfeld() {
-		return sp;
-	}
-	
-	public void setSpielfeld(Spielfeld sp) {
-		if(sp != null) {
-			this.sp = sp;
-		}
-	}
+    public Spielfeld getSpielfeld() {
+        return sp;
+    }
+    
+    public void setSpielfeld(Spielfeld sp) {
+        if(sp != null) {
+            this.sp = sp;
+        }
+    }
 }
 
 }
