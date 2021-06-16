cc8b301bfd85c5952b6ffdbd4502c6c448c6db8a
Leon Döhler <leondoehler@gmx.de>
2021-06-16 08:16:23 +0200

    Klasse Spielfeld

--- Spielfeld.java
+++ Spielfeld.java
@@ -1,33 +1,38 @@
-
-/**
- * Beschreiben Sie hier die Klasse Spielfeld.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
+ 
+ 
+ 
 public class Spielfeld
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Spielfeld
-     */
+    private Gebiet [][] feld;
+    
     public Spielfeld()
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        feld = new Gebiet [10] [10];
+        for (int x = 0; x < 10) ;
+        for (int y= 0; y < 10);
+        feld[x][y] = new Gebiet [10][10];
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
     {
-        // tragen Sie hier den Code ein
-        return x + y;
+        feld [1][2].setzeSchiff();
+        feld [2][2].setzeSchiff();
+        feld [2][3].setzeSchiff();
+        
+        
+        
+    
     }
+    {
+
+    
+
+
+    
+     
+    
+     
+     
+    
+     
+    
+    
 }
